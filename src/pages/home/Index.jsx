fd42113fb6176c12ff3a686cf51313ec61b93cfc
Sonia Appiah <sonia.appiah1@gmail.com>
2023-05-03 20:56:24 +0200

    Correction prettier

---
+++ src/pages/home/Index.jsx
@@ -0,0 +1,11 @@
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello there</p>
+      </header>
+    </div>
+  )
+}
+
+export default Home
