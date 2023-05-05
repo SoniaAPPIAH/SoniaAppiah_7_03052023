e5e0a35e530b351d93fac931b89f471f5f39dc68
Sonia Appiah <sonia.appiah1@gmail.com>
2023-05-05 16:28:08 +0200

    Installation React Router

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Home from './pages/home/Index.jsx';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Home from './pages/home/index.jsx'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <Home />
   </React.StrictMode>
-);
+)
